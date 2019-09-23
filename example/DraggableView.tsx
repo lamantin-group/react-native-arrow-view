155b8f545fe0215511c395638db14fac9897c9f3
whalemare <nanostet@gmail.com>
2019-09-23 18:41:21 +0700

    Add location events

--- example/DraggableView.tsx
+++ example/DraggableView.tsx
@@ -10,6 +10,7 @@ import {
   Dimensions,
 } from 'react-native'
 import { string } from 'prop-types'
+import { lchown } from 'fs'
 
 interface DraggableViewProps {
   style?: ViewStyle
@@ -36,6 +37,10 @@ export default class DraggableView extends Component<DraggableViewProps, Draggab
       pan: new Animated.ValueXY(),
       scale: new Animated.Value(1),
     }
+
+    if (props.refer) {
+      this.props.refer(this)
+    }
   }
 
   _panResponder: PanResponderInstance = null
@@ -53,7 +58,21 @@ export default class DraggableView extends Component<DraggableViewProps, Draggab
       },
 
       // When we drag/pan the object, set the delate to the states pan position
-      onPanResponderMove: Animated.event([null, { dx: this.state.pan.x, dy: this.state.pan.y }]),
+      onPanResponderMove: (event, gesture) => {
+        const { locationX, locationY } = event.nativeEvent
+        const dx = this.state.pan.x
+        const dy = this.state.pan.y
+        Animated.event([null, { dx: dx, dy: dy }])(event, gesture)
+        // console.warn('dx = ', dx, 'dy = ', dy)
+        // this.setState({
+        //   layoutX: dx._value,
+        //   layoutY: dy._value,
+        // })
+        this.setState({
+          x: locationX,
+          y: locationY,
+        })
+      },
 
       onPanResponderRelease: (e, { vx, vy }) => {
         // Flatten the offset to avoid erratic behavior
@@ -62,6 +81,9 @@ export default class DraggableView extends Component<DraggableViewProps, Draggab
       },
     })
   }
+
+  layoutX = 0
+  layoutY = 0
 
   render() {
     // Destructure the value of pan from the state
@@ -77,12 +99,29 @@ export default class DraggableView extends Component<DraggableViewProps, Draggab
     const rotate = '0deg'
 
     // Calculate the transform property and set it as a value for our style which we add below to the Animated.View component
-    const imageStyle = { transform: [{ translateX }, { translateY }, { rotate }, { scale }] }
+    const imageStyle = { transform: [{ translateX }, { translateY }, { rotate }] }
 
     return (
       <Animated.View style={imageStyle} {...this._panResponder.panHandlers}>
-        <View style={{ backgroundColor: '#ffcc00', borderRadius: 4, padding: 16 }}>
+        <View
+          style={{ height: 100, minWidth: 200, maxWidth: 200 }}
+          ref={ref => {
+            this.marker = ref
+          }}
+          onLayout={({ nativeEvent }) => {
+            if (this.marker) {
+              this.marker.measure((x, y, width, height, pageX, pageY) => {
+                this.setState({
+                  x: Math.round(pageX).toFixed(0),
+                  y: Math.round(pageY).toFixed(0),
+                })
+              })
+            }
+          }}
+          style={{ backgroundColor: '#ffcc00', borderRadius: 4, padding: 16 }}>
           <Text>{text}</Text>
+          <Text>x = {this.state.x}</Text>
+          <Text>y = {this.state.y}</Text>
         </View>
       </Animated.View>
     )
